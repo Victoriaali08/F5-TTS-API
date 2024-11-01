2f25aee6b59b0f42e146d19e50b498eb33d084e1
SamgeShao <samge720@gmail.com>
2024-11-01 01:10:13 +0000

    refresh这个参数不参与md5计算

--- src/utils/u_common.py
+++ src/utils/u_common.py
@@ -3,7 +3,7 @@ from src.models import InferenceRequest
 
 def generate_md5(inference_request: InferenceRequest) -> str:
     request_dict = inference_request.model_dump()
-    sorted_request_dict = {k: request_dict[k] for k in sorted(request_dict) if k not in ["stream", "remove_silence"]}
+    sorted_request_dict = {k: request_dict[k] for k in sorted(request_dict) if k not in ["stream", "refresh"]}
     json_bytes = str(sorted_request_dict).encode('utf-8')
     md5_hash = hashlib.md5(json_bytes).hexdigest()
     return md5_hash
